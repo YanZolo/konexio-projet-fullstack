d520df1f0f70ae987fa7c72dbdc11ed994e5752a
sofianeSghiouar <sofianesgh@gmail.com>
2021-08-05 14:48:33 +0200

    new update now ishrane gonna code

--- front/script.js
+++ front/script.js
@@ -1,47 +1,48 @@
 
-// $(function(){
+$(function(){
 
-//     $("#btnShowData").click(function(){
-//         $.ajax({
-//             url: "http://localhost:8000/all",
-//             success : getAllCountries()
-//         });
+    $("#btnShowData").click(function(){
+        $.ajax({
+            url: "http://localhost:8000/all",
+            success : getAllCountries
+        });
 
-//     })
+    })
 
-// })
+})
 
-// function getAllCountries(data, status, response) {
+function getAllCountries(data, status, response) {
     
-//     console.log(JSON.parse(data[0].name))
-//     console.log(response)
-//     let dataC = response[0].name
-//     $("#container").html(dataC)
-// }
+    console.log(JSON.parse(data[0].name))
+    console.log(response)
+    console.log(data)
+    let dataC = response[0].name
+    $("#container").html(dataC)
+}
 
-document.addEventListener("DOMContentLoaded", function () {
+// document.addEventListener("DOMContentLoaded", function () {
 
-    let btn = document.querySelector("#btnShowData");
-    let container = document.querySelector("#container");
-    btn.addEventListener("click", function () {
-        var request = new XMLHttpRequest();
+//     let btn = document.querySelector("#btnShowData");
+//     let container = document.querySelector("#container");
+//     btn.addEventListener("click", function () {
+//         var request = new XMLHttpRequest();
 
-        request.open('GET', 'https://restcountries.eu/rest/v2/name/france', true);
-        request.onload = function () {
-            if (this.status >= 200 && this.status < 400) {
-                let data = JSON.parse(this.response)
-                console.log(data[0].name);
-                console.log(data[0].capital);
-                container.innerHTML(data[0].name)
-            } else {
-                console.log('error return information')
-            }
-        };
-        request.onerror = function () {
-            console.log('error connection')
-        };
+//         request.open('GET', 'https://restcountries.eu/rest/v2/name/france', true);
+//         request.onload = function () {
+//             if (this.status >= 200 && this.status < 400) {
+//                 let data = JSON.parse(this.response)
+//                 console.log(data[0].name);
+//                 console.log(data[0].capital);
+//                 container.innerText(data[0].name)
+//             } else {
+//                 console.log('error return information')
+//             }
+//         };
+//         request.onerror = function () {
+//             console.log('error connection')
+//         };
 
-        request.send();
-    });
+//         request.send();
+//     });
 
-});
+// });
